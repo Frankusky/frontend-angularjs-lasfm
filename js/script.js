37aa8d2906a986f25d20891bb7b450b79e302a40
Frank Sam Hu <fsh1991@hotmail.com>
2015-01-30 12:02:50 -0600

    Design started

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 // Javascript Code.
-angular.module('app', [])
-.controller('PasswordController', function PasswordController($scope) {
-  $scope.title = "This is AngularJS and Jasmin!";
-  $scope.password = '';
-  $scope.grade = function() {
-    var size = $scope.password.length;
-    if (size > 8) {
-      $scope.strength = 'strong';
-    } else if (size > 3) {
-      $scope.strength = 'medium';
-    } else {
-      $scope.strength = 'weak';
-    }
-  };
+//para hacer uso de $resource debemos colocarlo al crear el modulo
+
+//Starting angular
+var angularTesting = angular.module('angularTesting', []);
+
+//Getting only the url of music object
+angularTesting.filter("soloUrl", function(){
+	return function(item){
+		return (JSON.stringify(item)).slice(10,-18);
+	};
+});
+
+//Getting json
+angularTesting.controller('jsonData', function ($scope, $http) {
+	
+		$http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=85b8c37b1a6be5182a5ed0549c4a7400&format=json').success(function(data) {
+			$scope.track = data.tracks.track;
+		});
+		$scope.ordenarPor = function(orden) {
+			$scope.ordenSeleccionado = orden;
+		};
+	
+	
 });
