8b4d6d4a9802a7d5cc5d8a7f69305be4557ada25
Frank Sam Hu <fsh1991@hotmail.com>
2015-01-31 21:24:28 -0600

    Login/sign up working, redirection after login now working

--- js/script.js
+++ js/script.js
@@ -13,7 +13,46 @@ var angularTesting = angular.module('angularTesting', ["ngRoute"]).config(functi
 	.otherwise({
 		redirectTo: '/'
 	});
-});
+}).run(['$rootScope', "$location", function($scope, $location) {
+	$scope.currentUser = Parse.User.current();
+	
+	$scope.signUp = function(form) {
+		var user = new Parse.User();
+		user.set("email", form.email);
+		user.set("username", form.username);
+		user.set("password", form.password);
+		
+		user.signUp(null, {
+			success: function(user) {
+				$scope.currentUser = user;
+				$scope.$apply();
+			},
+			error: function(user, error) {
+				alert("Unable to sign up:  " + error.code + " " + error.message);
+			}
+		});
+	};
+	
+	$scope.logIn = function(form, newPath) {
+		Parse.User.logIn(form.username, form.password, {
+			success: function(user) {
+				
+				$scope.currentUser = user;
+				$scope.$apply();
+				$location.path(newPath);
+			},
+			error: function(user, error) {
+				alert("Unable to log in: " + error.code + " " + error.message);
+			}
+		});
+	};
+	
+	$scope.logOut = function(form) {
+		Parse.User.logOut();
+		$scope.currentUser = null;
+	};
+}]);
+
 
 //Getting only the url of music object
 angularTesting.filter("soloUrl", function(){
@@ -34,42 +73,3 @@ angularTesting.controller('jsonData', function ($scope, $http) {
 		};
 });
 
-angular.module('AuthApp', [])
-.run(['$rootScope', function($scope) {
-	$scope.scenario = 'Sign up';
-	$scope.currentUser = Parse.User.current();
-	
-	$scope.signUp = function(form) {
-		var user = new Parse.User();
-		user.set("email", form.email);
-		user.set("username", form.username);
-		user.set("password", form.password);
-		
-		user.signUp(null, {
-			success: function(user) {
-				$scope.currentUser = user;
-				$scope.$apply();
-			},
-			error: function(user, error) {
-				alert("Unable to sign up:  " + error.code + " " + error.message);
-			}
-		});    
-	};
-	
-	$scope.logIn = function(form) {
-		Parse.User.logIn(form.username, form.password, {
-			success: function(user) {
-				$scope.currentUser = user;
-				$scope.$apply();
-			},
-			error: function(user, error) {
-				alert("Unable to log in: " + error.code + " " + error.message);
-			}
-		});
-	};
-	
-	$scope.logOut = function(form) {
-		Parse.User.logOut();
-		$scope.currentUser = null;
-	};
-}]);
